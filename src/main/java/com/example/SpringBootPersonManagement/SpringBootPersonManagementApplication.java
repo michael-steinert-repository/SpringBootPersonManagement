3c48156a297f158ed78008c5b856eec74fe684fe
Michael Steinert <steinert-michael@gmx.de>
2020-03-22 12:55:58 +0100

    first implementation of api

---
+++ src/main/java/com/example/SpringBootPersonManagement/SpringBootPersonManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootPersonManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPersonManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPersonManagementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/SpringBootPersonManagement/service/PersonService.java
@@ -0,0 +1,28 @@
+package com.example.SpringBootPersonManagement.service;
+
+import com.example.SpringBootPersonManagement.dao.PersonDao;
+import com.example.SpringBootPersonManagement.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person){
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+}
